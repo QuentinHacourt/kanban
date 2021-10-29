79ba59d67724c4c462c81a9f9fee0bd182f6b2dc
QuentinHacourt <quentin.hacourt@student.ucll.be>
2021-10-29 17:52:50 +0200

    updated router

--- backend/router/router.go
+++ backend/router/router.go
@@ -18,32 +18,32 @@ func Router() *mux.Router {
 	// ===========
 
 	// stories
-	router.HandleFunc("/story/{id}", middleware.GetStory).Methods("GET", "OPTIONS")
-	router.HandleFunc("/story", middleware.GetAllStories).Methods("GET", "OPTIONS")
+	router.HandleFunc("/story/{id}", middleware.GetStory).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/story", middleware.GetAllStories).Methods(http.MethodGet, http.MethodOptions)
 	router.HandleFunc("/story", middleware.CreateStory).Methods(http.MethodPost, http.MethodOptions)
-	router.HandleFunc("/story/{id}", middleware.UpdateStory).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/story/{id}", middleware.DeleteStory).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/story/{id}", middleware.UpdateStory).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/story/{id}", middleware.DeleteStory).Methods(http.MethodDelete, http.MethodOptions)
 
 	// developers
-	router.HandleFunc("/developer/{id}", middleware.GetDeveloper).Methods("GET", "OPTIONS")
-	router.HandleFunc("/developer", middleware.GetAllDevelopers).Methods("GET", "OPTIONS")
-	router.HandleFunc("/developer", middleware.CreateDeveloper).Methods("POST", "OPTIONS")
-	router.HandleFunc("/developer/{id}", middleware.UpdateDeveloper).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/developer/{id}", middleware.DeleteDeveloper).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/developer/{id}", middleware.GetDeveloper).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/developer", middleware.GetAllDevelopers).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/developer", middleware.CreateDeveloper).Methods(http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/developer/{id}", middleware.UpdateDeveloper).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/developer/{id}", middleware.DeleteDeveloper).Methods(http.MethodDelete, http.MethodOptions)
 
 	// projecs
-	router.HandleFunc("/project/{id}", middleware.GetProject).Methods("GET", "OPTIONS")
-	router.HandleFunc("/project", middleware.GetAllProjects).Methods("GET", "OPTIONS")
-	router.HandleFunc("/project", middleware.CreateProject).Methods("POST", "OPTIONS")
-	router.HandleFunc("/project/{id}", middleware.UpdateProject).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/project/{id}", middleware.DeleteProject).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/project/{id}", middleware.GetProject).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/project", middleware.GetAllProjects).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/project", middleware.CreateProject).Methods(http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/project/{id}", middleware.UpdateProject).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/project/{id}", middleware.DeleteProject).Methods(http.MethodDelete, http.MethodOptions)
 
 	// teams
-	router.HandleFunc("/team/{id}", middleware.GetTeam).Methods("GET", "OPTIONS")
-	router.HandleFunc("/team", middleware.GetAllTeams).Methods("GET", "OPTIONS")
-	router.HandleFunc("/team", middleware.CreateTeam).Methods("POST", "OPTIONS")
-	router.HandleFunc("/team/{id}", middleware.UpdateTeam).Methods("PUT", "OPTIONS")
-	router.HandleFunc("/team/{id}", middleware.DeleteTeam).Methods("DELETE", "OPTIONS")
+	router.HandleFunc("/team/{id}", middleware.GetTeam).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/team", middleware.GetAllTeams).Methods(http.MethodGet, http.MethodOptions)
+	router.HandleFunc("/team", middleware.CreateTeam).Methods(http.MethodPost, http.MethodOptions)
+	router.HandleFunc("/team/{id}", middleware.UpdateTeam).Methods(http.MethodPut, http.MethodOptions)
+	router.HandleFunc("/team/{id}", middleware.DeleteTeam).Methods(http.MethodDelete, http.MethodOptions)
 
 	return router
 }
